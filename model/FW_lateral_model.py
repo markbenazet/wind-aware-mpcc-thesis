cfb47b620eddb675cec69d25ad339e67270041ae
Mark Benazet <mbenazet@ethz.ch>
2024-06-25 22:12:10 +0200

    initial commit, thesis start: Kinematic Model acados tryout

---
+++ model/FW_lateral_model.py
@@ -0,0 +1,52 @@
+from acados_template import AcadosModel
+import casadi as cs
+
+class FixedWingLateralModel():
+    def __init__(self):
+        
+        self.model_name = 'fixed_wing_lateral_model'
+
+        # #constants
+        # self.mass = 1.0
+        # self.max_thrust = 1.0
+
+    def fixed_wing_lateral_model(self) -> AcadosModel:
+
+        # State variables
+        x = cs.MX.sym('x')
+        y = cs.MX.sym('y')
+        V = cs.MX.sym('V')
+        yaw = cs.MX.sym('yaw')
+
+        # Input variables
+        a_x = cs.MX.sym('a_x')
+        a_y = cs.MX.sym('a_y')
+        yaw_rate = cs.MX.sym('yaw_rate')
+
+        # DState and Input vectors
+        states = cs.vertcat(x, y, V, yaw)
+        controls = cs.vertcat(a_x, a_y, yaw_rate)
+
+        # Define the dynamics equations
+        dx_dt = V * cs.cos(yaw)
+        dy_dt = V * cs.sin(yaw)
+        dV_dt = a_x
+        dyaw_dt = yaw_rate
+
+        # Concatenate the state derivatives
+        state_derivatives = cs.vertcat(dx_dt, dy_dt, dV_dt, dyaw_dt)
+
+        # Create an AcadosModel object
+        model = AcadosModel()
+        model.f_expl_expr = state_derivatives
+        model.x = states
+        model.u = controls
+        model.name = self.model_name
+
+        return model
+
+if __name__ == "__main__":
+    # Create an instance of the FixedWingLateralModel class
+    fw_model = FixedWingLateralModel()
+    model = fw_model.fixed_wing_lateral_model()
+    print("AcadosModel created with name:", model.name)
